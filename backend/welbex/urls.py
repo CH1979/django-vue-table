e55e2651e0ebb86dad0c5bc843a4e359d01441ff
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-04-16 15:08:55 +0300

    Add frontend

--- backend/welbex/urls.py
+++ backend/welbex/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 from django.urls import path, include
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
 ]
